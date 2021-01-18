b57cffbedba295721db61ad1c5341780f3462831
RyanSaxe <ryancsaxe+1@gmail.com>
2021-01-18 16:03:28 -0500

    comments and footnotes style

--- _plugins/footnote-detail.rb
+++ _plugins/footnote-detail.rb
@@ -1,7 +1,7 @@
 module Jekyll
     module AssetFilter
       def fndetail(input, id)
-        "<li id='fn-#{id}'>#{input}<a href='#fnref-#{id}' class='footnote footnotes'>↩</a></li></div>"
+        "<li id='fn-#{id}'>#{input}<a href='#fnref-#{id}' class='footnote footnotes'>↩</a></li>"
       end
     end
   end
